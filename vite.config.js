e2d3c92fd32c79fea8478933dc62c2ef8e7ce150
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-05-01 13:17:29 -0700

    feat: upgrade typescript, vite, vite-tsconfig-paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 const reactRefreshPlugin = require("@vitejs/plugin-react-refresh").default;
 const viteTsConfigPathsPlugin = require("vite-tsconfig-paths").default;
 const macrosPlugin = require("vite-plugin-babel-macros").default;
-const xmlhttprequest = require("xmlhttprequest");
 const { defineConfig, loadEnv } = require("vite");
 
 /**

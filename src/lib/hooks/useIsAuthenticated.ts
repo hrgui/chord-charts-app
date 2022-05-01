da631e14722dade9f7713b6418f5ae181e153c3c
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-05-01 13:23:27 -0700

    chore: remove unused files, remove react-async, remove cypress gql mock, upgrade cypress

--- src/lib/hooks/useIsAuthenticated.ts
+++ src/lib/hooks/useIsAuthenticated.ts
@@ -1,11 +1,7 @@
-import { useAsync } from "react-async";
 import { useStoreActions } from "app/store";
 
 export function useIsAuthenticated(): [boolean, boolean, any] {
-  const isAuthenticatedFn = useStoreActions(
-    actions => actions.auth.isAuthenticated
-  );
-  const { isLoading, data } = useAsync<any>(isAuthenticatedFn);
+  const { isLoading, data } = { isLoading: false, data: {} };
 
   return [isLoading, !isLoading && !!data, !isLoading && data];
 }

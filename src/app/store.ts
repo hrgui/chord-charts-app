3bbdaa6b07a85684120665ad2f6a0a9e9a34bf5b
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-05-01 19:58:33 -0700

    feat: RTK-query sample

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "app/counter/counterSlice";
 import uiStateReducer from "./core/uiStateSlice";
 import darkModeMiddlware from "app/actions/darkModeMiddleware";
+import { pokemonApi } from "./counter/pokemon";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     uiState: uiStateReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().prepend(darkModeMiddlware.middleware),
+    getDefaultMiddleware().prepend(darkModeMiddlware.middleware).concat(pokemonApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself

cffa395eebc29f510fe4bc8e0ee02a83e21b7883
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-19 20:30:22 -0700

    chore: upgrade to vite 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  safelist: [
-    {
-      pattern: /./,
-    },
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
     extend: {
